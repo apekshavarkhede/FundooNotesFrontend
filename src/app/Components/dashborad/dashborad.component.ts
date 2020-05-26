102160dea8a7b7f70111e6272d8a2c3f27225325
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-05-26 16:27:21 +0530

    Add: place all icons on toolBar and add signout functionality

--- src/app/Components/dashborad/dashborad.component.ts
+++ src/app/Components/dashborad/dashborad.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../Services/data.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashborad',
@@ -7,11 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboradComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService: DataService, private router: Router) { }
+  open: boolean
+  title = "fundooNotes";
+  grid:boolean=false;
 
-  title="fundoo"
 
   ngOnInit() {
+    this.dataService.open.subscribe(value => this.open = value)
   }
+
+  signOut() {
+    sessionStorage.clear()
+    this.router.navigate(["/login"]);
+  }
+
+
 
 }
