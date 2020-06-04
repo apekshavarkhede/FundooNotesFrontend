773c39a5743365b9ce28f3cf25fdd914704a1fe0
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-06-04 17:28:48 +0530

    Add: search note by title

--- src/app/Components/notes/notes.component.ts
+++ src/app/Components/notes/notes.component.ts
@@ -22,27 +22,14 @@ export class NotesComponent implements OnInit {
     , private noteService: NoteService) { }
 
   getallNotes(value) {
-
     this.noteService.getAlNotes().subscribe(
       (response: any) => {
         this.totalNotes = response.data
+        let notes = this.totalNotes.filter((element) => {
+          return element.isArchive === false
+        })
+        this.totalNotes = notes;
         this.totalNotes.reverse()
-        // console.log("value===", value);
-        // let matchedNotes = []
-        if (value) {
-          let note = this.totalNotes.filter((element) => {
-            return element.title === value
-          })
-          this.totalNotes = note;
-        }
-        else {
-          let notes = this.totalNotes.filter((element) => {
-            console.log("element.isArchive !== true", element, element.isArchive !== true);
-            return element.isArchive === false
-          })
-          console.log("notes", notes);
-          this.totalNotes = notes;
-        }
       },
       err => {
         this.snackBar.open("err", "end now")
@@ -50,17 +37,31 @@ export class NotesComponent implements OnInit {
     )
   }
 
+
+  searchNoteByTitle(data) {
+    let noteData = {
+      search: data
+    }
+    this.noteService.searchNote(noteData).subscribe(response => {
+      this.totalNotes = response.data
+      let notes = this.totalNotes.filter((element) => {
+        return element.isArchive === false
+      })
+      this.totalNotes = notes;
+      this.totalNotes.reverse()
+    })
+  }
+
   ngOnInit() {
     this.dataService.searchNote.subscribe(response => {
       this.x = response;
-      console.log("response in ngOnit", this.x);
-
-      this.getallNotes(this.x)
+      if (response.length > 0) {
+        this.searchNoteByTitle(response)
+      }
+      if (response.length === 0) {
+        this.getallNotes(this.x)
+      }
     })
-    // let xx = this.dataService.searchNote.subscribe(
-    //   m => this.x = m
-    // )
-    console.log("xxx===", this.x);
   }
 
 
