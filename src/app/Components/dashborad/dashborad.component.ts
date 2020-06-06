41260f33f75c9e3f0cbd985b3231603cd94ecefc
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-06-06 13:29:30 +0530

    Add: change title on toolbar when component is changed

--- src/app/Components/dashborad/dashborad.component.ts
+++ src/app/Components/dashborad/dashborad.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../Services/data.service'
 import { Router } from '@angular/router';
 import { NoteService } from '../../Services/note.service'
+import { MatDialog } from '@angular/material';
+import { LabelDialougeComponent } from '../label-dialouge/label-dialouge.component'
 
 @Component({
   selector: 'app-dashborad',
@@ -10,15 +12,16 @@ import { NoteService } from '../../Services/note.service'
 })
 export class DashboradComponent implements OnInit {
 
-  constructor(private dataService: DataService, private router: Router, private noteService: NoteService) { }
+  constructor(private dataService: DataService, private router: Router, private noteService: NoteService, public dialog: MatDialog) { }
   open: boolean
-  title = "fundooNotes";
+  title;
   grid: boolean = false;
   inputValue: string;
   searchNote: any;
   labels: Array<any> = [];
 
   ngOnInit() {
+    this.title = this.dataService.title.subscribe(response => this.title = response)
     this.dataService.lable.subscribe(res => {
       this.getAllLabels()
     })
@@ -32,8 +35,8 @@ export class DashboradComponent implements OnInit {
   getAllLabels() {
     this.noteService.getLabels().subscribe(response => {
       response.data.forEach(element => {
-        if (!this.labels.includes(element.label)) {
-          this.labels.push(element.label)
+        if (!this.labels.includes(element.labelName)) {
+          this.labels.push(element.labelName)
         }
       });
     })
@@ -41,6 +44,13 @@ export class DashboradComponent implements OnInit {
 
   searchKey() {
     this.dataService.searchInput(this.inputValue)
+  }
+
+  editLabels() {
+    this.dialog.open(LabelDialougeComponent, {
+      minHeight: '250px',
+      width: '260px',
+    })
   }
 
 }
