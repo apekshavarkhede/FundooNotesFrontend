ed99657e4d74fd0c3527a122c8281d4bcaa3c73f
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-06-11 10:53:17 +0530

    Add: testCase: improper userData gives validation error

--- src/app/Components/register/register.component.spec.ts
+++ src/app/Components/register/register.component.spec.ts
@@ -8,24 +8,20 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, By } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
-
 import { MatSnackBar, MatSnackBarModule } from '@angular/material';
 import { MatInputModule, MatListModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
-
 import { RouterTestingModule } from '@angular/router/testing';
-// import { AppComponent } from 'src/app/app.component';
-// import {FormGroup} from '@angular/forms'
+import { DebugElement } from '@angular/core';
 
 fdescribe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
-  let userService: UserService
+  let userService: UserService;
+  let input: HTMLElement;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -55,13 +51,15 @@ fdescribe('RegisterComponent', () => {
     userService = TestBed.get(UserService)
     component = fixture.componentInstance;
     fixture.detectChanges();
+    de = fixture.debugElement.query(By.css('form'));
+    input = fixture.nativeElement.querySelector('input')
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  fit('should check user is created or not', () => {
+  it('should check user is created or not', () => {
     let registration = component.next()
     let response = { "data": "mail sent check your email", "status": true }
     let result;
@@ -73,14 +71,22 @@ fdescribe('RegisterComponent', () => {
       "password": "Apeksha1234"
     }
 
-    spyOn(userService, 'register').and.returnValue(of(response)) 
+    spyOn(userService, 'register').and.returnValue(of(response))
     userService.register(user).subscribe(res => {
       result = res
     })
 
     expect(userService.register).toHaveBeenCalled()
-    expect(result.data).toBe('mail sent check your email')    
+    expect(result.data).toBe('mail sent check your email')
   })
-  
+
+  fit('given improper userData when check for valid should return false', () => {
+    component.formData.controls['firstName'].setValue('');
+    component.formData.controls['lastName'].setValue('');
+    component.formData.controls['userEmail'].setValue('');
+    component.formData.controls['password'].setValue('')
+    expect(component.formData.valid).toBeFalsy()
+  })
+
 });
 
